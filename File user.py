c742240986639ae77cdfd7a6b521ac0315dc239a
Ayush Srivastava <ayush2003somu@gmail.com>
2023-09-04 00:50:48 +0530

    user defined txt file

--- File user.py
+++ File user.py
@@ -6,14 +6,14 @@ i3=input("Second one here:")
 i4=input("Third coloumn")
 i5=input("And the last one:")
 x=[i2, i3, i4 , i5]
-file=open("file3.txt","w")# this line is for creating the text file
+file=open("file4.txt","w")# this line is for creating the text file
 #we will use 'for' loop for writing list
 for i in x:
-    file = open("file3.txt", "w")
+    
     file.write(i+ "\n") #command for writing in text file.
-    #file.close()
-    file=open("file3.txt","r")
-    x1=file.read()
-    print(x1)
+    
+file=open("file4.txt","r")
+x1=file.read()
+print(x1)    
 file.close()
 #Always close the file.
