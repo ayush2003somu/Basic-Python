55f40599d045ed0db1e3e654e1570214fc447a1f
Ayush Srivastava <ayush2003somu@gmail.com>
2023-11-03 22:11:18 +0530

    auction page

---
+++ auction.py
@@ -0,0 +1,4 @@
+l1={"Mercury":0.378,"Venus":0.907,"Moon":0.166,"Mars":0.377,"Io":0.1835,"Europa":0.1335,"Ganymede":0.1448,"Callisto":0.1264}
+i1=input("""Enter the planet on which the astraunaut is to be travelled:
+         
+         """)
