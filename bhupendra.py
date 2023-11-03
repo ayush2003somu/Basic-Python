c889a5e2025b73456949326972f3186e16c62aec
Ayush Srivastava <ayush2003somu@gmail.com>
2023-11-03 22:11:45 +0530

    Bhupendra Jogi

---
+++ bhupendra.py
@@ -0,0 +1,19 @@
+while 1>0:
+    _name_= input("Reporter question 1: ").lower()
+    if _name_ == "kya naam bataya apna":
+        print("Bhupendra Jogi")
+        
+    else:
+        print("GALAT QUESTION")    
+    name2= input("Reporter question 2:").lower()
+    if name2== "us me kaha kaha gye ho":
+
+        print("US me Bahut jagah Gye hai")
+    else:
+        print("GALAT QUESTION")    
+
+    name3=input("Reporter question 3:").lower()
+    if name3== "naam batao":
+        print("Bhupendra jogi")
+    else:
+        print("GALAT QUESTIONn")    
